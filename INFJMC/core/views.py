7f34771b5c0d6fbb88df3767baf0656de4d63b19
AlumnosInf <AlumnosInf@soportetiu.local>
2023-04-28 11:59:08 -0400

    Pagina core

--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    #return HttpResponse("<h1>Home</h1>")
+    return render(request, 'core/home.html')
+    
+def docente(request):
+    return render(request, 'core/docente.html')
+def carrera(request):
+    return render(request, 'core/carrera.html')
